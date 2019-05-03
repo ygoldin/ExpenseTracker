2e565960228c75c638c9254cf6a565a8841bf4aa
Yael Goldin <ygoldin@cs.washington.edu>
2019-05-03 12:29:43 -0700

    Expense created

---
+++ Expense.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include "./Expense.h"
+
+using namespace std;
+
+Expense::Expense(double cost, string payer, unordered_set<string> &participants)
+    : Expense(cost, payer, participants, true) { }
+
+Expense::Expense(double cost, string payer, unordered_set<string> &participants,
+                 bool payerInvolved) {
+  cost_ = cost;
+  payer_ = payer;
+  payer_involved_ = payerInvolved;
+  participants_ = new unordered_set<string>();
+  for (string p : participants) {
+    participants_->insert(p);
+  }
+}
+
+Expense::~Expense() {
+  delete participants_;
+}
+
+double Expense::IndividualCost() const {
+  int size = participants_->size();
+  if (payer_involved_) {
+    size++;
+  }
+  return cost_ / size;
+}
---
+++ Expense.h
@@ -0,0 +1,35 @@
+#ifndef EXPENSETRACKER_EXPENSE_H
+#define EXPENSETRACKER_EXPENSE_H
+
+
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+class Expense {
+  public:
+    // creates an expense where the payer is involved (i.e. didn't just pay for someone else)
+    Expense(double cost, string payer, unordered_set<string> &participants);
+
+    // creates an expense
+    Expense(double cost, string payer, unordered_set<string> &participants, bool payerInvolved);
+
+    // destructs the expense
+    ~Expense();
+
+    double Cost() const { return cost_;}
+    string Payer() const { return payer_; }
+    unordered_set<string> Participants() const { return *participants_; }
+
+    // returns how much each person's cost was in the expense
+    double IndividualCost() const;
+
+  private:
+    double cost_;
+    string payer_;
+    unordered_set<string>* participants_;
+    bool payer_involved_;
+};
+
+#endif //EXPENSETRACKER_EXPENSE_H
