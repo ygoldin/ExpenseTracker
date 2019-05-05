d9da92c5965c44453dc6c6e689ddf7b441426f18
Yael Goldin <ygoldin@cs.washington.edu>
2019-05-04 23:51:31 -0700

    individual balance table

---
+++ BalanceTable.cpp
@@ -0,0 +1,22 @@
+// Created by Yael Goldin
+
+#include <unordered_map>
+#include <string>
+#include "BalanceTable.h"
+
+BalanceTable::BalanceTable() {
+  balances_ = new unordered_map<string, double>();
+}
+
+BalanceTable::~BalanceTable() {
+  delete balances_;
+}
+
+void BalanceTable::UpdateBalance(string person, double amount) {
+  if (balances_->count(person)) {
+    balances_->insert({person, amount});
+  } else {
+    double total = balances_->at(person) + amount;
+    balances_->insert({person, total});
+  }
+}
---
+++ BalanceTable.h
@@ -0,0 +1,25 @@
+// Created by Yael Goldin
+
+#ifndef EXPENSETRACKER_BALANCETABLE_H
+#define EXPENSETRACKER_BALANCETABLE_H
+
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+// this class represents one person's current balances
+class BalanceTable {
+  public:
+    BalanceTable();
+    ~BalanceTable();
+
+    // either creates a new balance with this person, or updates it by adding the given amount
+    void UpdateBalance(string person, double amount);
+
+  private:
+    unordered_map<string, double>* balances_;
+};
+
+
+#endif //EXPENSETRACKER_BALANCETABLE_H
