c2489fe5972aade10e96b4eb8f6bead955b2cc6c
Yael Goldin <ygoldin@cs.washington.edu>
2019-05-04 23:24:18 -0700

    Balance updated

--- Balance.cpp
+++ Balance.cpp
@@ -1,16 +1,7 @@
 // Created by Yael Goldin
 
-#include <string>
 #include "Balance.h"
-
-using namespace std;
 
 void Balance::Update(double amount) {
   amount_ += amount;
-  if (amount_ < 0) {
-    amount_ = -amount_;
-    string temp = receiver_;
-    receiver_ = debtor_;
-    debtor_ = temp;
-  }
 }
--- Balance.h
+++ Balance.h
@@ -9,21 +9,17 @@ using namespace std;
 
 class Balance {
   public:
-    Balance(double amount, string receiver, string debtor): amount_(amount), receiver_(receiver),
-        debtor_(debtor) { }
+    Balance(double amount, string otherParty): amount_(amount), other_party_(otherParty) { }
 
     double Amount() const { return amount_; }
-    string Receiver() const { return receiver_; }
-    string Debtor() const { return debtor_; }
+    string OtherParty() const { return other_party_; }
 
     // adds the given amount to the balance
-    // if the total amount goes negative, switches the receiver and debtor
     void Update(double amount);
 
   private:
     double amount_;
-    string receiver_;
-    string debtor_;
+    string other_party_;
 };
 
 
